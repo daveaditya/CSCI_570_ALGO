ff960d94aa2d349473d0273735ccc4934544092a
Aditya Dave <aditya.dave2017@gmail.com>
2021-12-05 22:58:14 -0800

    implemented DC according to Tardos. Still in testing.

--- src/edu/usc/algorithm_design/SequenceAlignment.java
+++ src/edu/usc/algorithm_design/SequenceAlignment.java
@@ -16,28 +16,31 @@ public class SequenceAlignment {
     private int[][] dp = null;
     private List<Pair> P = null;
 
+
+    public List<Pair> getP(){
+        return this.P;
+    }
+
+
     public SequenceAlignment(String X, String Y) {
         this.dp = new int[X.length()][Y.length()];
         this.P = new ArrayList<>();
     }
 
-    public int alignment(String X, String Y){
-        System.out.println("First Base: " + X);
-        System.out.println("Second Base: " + Y);
-        StringBuilder result = new StringBuilder();
 
-        int xLength = X.length();
-        int yLength = Y.length();
+    public int alignment(String X, String Y) {
+        int m = X.length();
+        int n = Y.length();
 
-        for (int i = 0; i < xLength; i++) {
+        for (int i = 0; i < m; i++) {
             this.dp[i][0] = i * SequenceAlignment.GAP_PENALTY;
         }
-        for (int j = 0; j < yLength; j++) {
+        for (int j = 0; j < n; j++) {
             this.dp[0][j] = j * SequenceAlignment.GAP_PENALTY;
         }
 
-        for (int i = 1; i < xLength; i++) {
-            for (int j = 1; j < yLength; j++) {
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 this.dp[i][j] = Math.min(
                         Math.min(
                                 SequenceAlignment.GAP_PENALTY + this.dp[i - 1][j], SequenceAlignment.GAP_PENALTY + this.dp[i][j - 1]
@@ -45,135 +48,17 @@ public class SequenceAlignment {
             }
         }
 
-        System.out.println("Printing DP");
-        for (int i = 0; i < xLength; i++) {
-            for (int j = 0; j < yLength; j++) {
-                System.out.format("%6d",dp[i][j]);
-            }
-            System.out.println();
-        }
-
-        System.out.println("Final alignment cost :: " + dp[xLength-1][yLength-1]);
-        return dp[xLength-1][yLength-1];
-    }
-
-    /** Divide and Conquer - Efficient Implementation
-     * @param X
-     * @param Y
-     * @return
-     */
-    public void divideAndConquerAlignment(String X, String Y) {
-        int m = X.length();
-        int n = Y.length();
-
-//        String xFlip= null, yFlip= null;
-//        StringBuilder str = new StringBuilder();
-//        str.append(X);
-//        xFlip = String.valueOf(str.reverse());
-//
-//        StringBuilder str1 = new StringBuilder();
-//        str1.append(Y);
-//        yFlip = String.valueOf(str1.reverse());
-
-        if(m <= 2 || n <= 2){
-            this.alignment(X,Y);
-        }
-
-        int[][] left = spaceEfficientAlignment(X, Y.substring(0, n/2));
-        int[][] right = backwardSpaceEfficientAlignment_new(X, Y.substring(n/2+1, n));
-//        int[] right = spaceEfficientAlignment(xFlip, yFlip.substring(0, n/2));
-
-
-        int min = left[1][1] + right[1][0];
-        int best_q = 1;
-        for (int q = 2; q < m ; q++) {
-            if (min > left[q][1] + right[q][0]){
-                min = left[q][1] + right[q][0];
-                best_q = q - 1;
-            }
-        }
-//        int best_cost = Integer.MAX_VALUE;
-//        int best_q = m;
-//        for (int q = 0; q < n ; q++) {
-//            int cost = left[q] + right[q+1];
-//            if(cost < best_cost) {
-//                best_cost = cost;
-//                best_q = q;
+//        System.out.println("Printing DP");
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.format("%6d",dp[i][j]);
 //            }
+//            System.out.println();
 //        }
 
-//        System.out.println("Best cost :: " + best_cost);
-        System.out.println("Best q :: " + best_q);
-        P.add(new Pair(best_q, n/2));
-        divideAndConquerAlignment(X.substring(0,best_q), Y.substring(0, n/2));
-        divideAndConquerAlignment(X.substring(best_q+1,n), Y.substring(n/2 + 1, n));
+//        System.out.println("Final alignment cost :: " + dp[m-1][n-1]);
+        return dp[m-1][n-1];
     }
-
-    public List<Pair> getP(){
-        return this.P;
-    }
-
-//    public void divideAndConquerAlignment_Lecture(String X, String Y) {
-//        int x = X.length();
-//        int y = Y.length();
-//        String xFlip= null, yFlip= null;
-//        StringBuilder str = new StringBuilder();
-//        str.append(X);
-//        xFlip = String.valueOf(str.reverse());
-//
-//        StringBuilder str1 = new StringBuilder();
-//        str1.append(Y);
-//        yFlip = String.valueOf(str1.reverse());
-//
-//        // call alignment for Xleft to all of Y
-//        // call alignment for Xright (flip) and Xright (flip)
-//        // match the cost of alignment - that point is the DP point on Y.
-//        int[] xleft_cost;
-//        int[] xright_cost;
-//        xleft_cost = spaceEfficientAlignment(X.substring(0,x/2), Y);   // gives opt value of xleft and all of Y
-//        xright_cost = spaceEfficientAlignment(xFlip.substring(0, x/2) , yFlip);  // gives opt value of xright and all of Y flipped
-//        System.out.println("Xleft__ " + xleft_cost);
-//        System.out.println("Xright__ " + xright_cost);
-//
-//
-//
-//
-//
-//
-//
-////        if(x <= 2 || y <= 2){
-////            this.alignment(X,Y);
-////        }
-////        System.out.println(" Space Efficient alignment :: " + alignment(X,Y));
-////
-////
-////        System.out.println("sa 1 :: " + X);
-////        System.out.println("\n sa y :: " + Y.substring(0,y/2));
-////
-////        System.out.println("\n sa 2 :: " + xFlip);
-////        System.out.println("\n yFlip only:: " + yFlip);
-////        System.out.println("\n sa 2 :: " + yFlip.substring(0,y/2));
-////
-////
-////        spaceEfficientAlignment(X, Y.substring(0, y/2));
-////        spaceEfficientAlignment(xFlip, yFlip.substring(0, y/2));
-////
-////        int best_cost = Integer.MAX_VALUE;
-////        int best_q = x; // value for best_q
-////        for (int q = 0; q <= x ; q++) {
-////
-////            int cost = Integer.parseInt(Y.substring(q) + Y.substring(q+1)); //post flip check q for Y
-////            if(cost < best_cost) {
-////                best_cost = cost;
-////                best_q = q;
-////            }
-////        }
-////        System.out.println(best_q);
-////        P.add(best_q,y/2);
-////        System.out.println("P == " + P);
-////        divideAndConquerAlignment(X.substring(1,best_q), Y.substring(1, y/2));
-////        divideAndConquerAlignment(X.substring(1,best_q), Y.substring(1, y/2));
-//    }
 
 
     /**
@@ -186,87 +71,90 @@ public class SequenceAlignment {
         int n = Y.length();
 
         int[][] B = new int[m][2];
-        for (int j =1; j < m ; j++){
-            B[j][0] = j * SequenceAlignment.GAP_PENALTY;
+        for (int i =1; i < m ; i++){
+            B[i][0] = i * SequenceAlignment.GAP_PENALTY;
         }
 
+        // Find cost for X and Y
         for (int j = 1; j < n; j++) {
             B[0][1] = j * SequenceAlignment.GAP_PENALTY;
             for (int i = 1; i < m; i++) {
-                B[i][1] = Math.min(Math.min(SequenceAlignment.MISMATCH_COST[ALPHABETS.indexOf(X.charAt(i - 1))][ALPHABETS.indexOf(Y.charAt(j - 1))] + B[i - 1][0],
-                        SequenceAlignment.GAP_PENALTY + B[i - 1][1]), SequenceAlignment.GAP_PENALTY + B[i][0]);
+                B[i][1] = Math.min(
+                        Math.min(
+                                SequenceAlignment.MISMATCH_COST[ALPHABETS.indexOf(X.charAt(i))][ALPHABETS.indexOf(Y.charAt(j))] + B[i - 1][0], SequenceAlignment.GAP_PENALTY + B[i - 1][1]
+                        ), SequenceAlignment.GAP_PENALTY + B[i][0]);
             }
+
+            // Swap the columns, to be ready for next iteration
             for (int i = 1; i < m; i++) {
                 B[i][0] = B[i][1];
             }
         }
 
         return B;
-
-//        int[] R = new int[m];
-//        for (int i = 0; i < m; i++) {
-//            R[i] = B[i][1];
-//        }
-//        return R;
-    }
-
-    public int[] backwardSpaceEfficientAlignment(String X, String Y) {
-        int m = X.length();
-        int n = Y.length();
-
-        int[][] B = new int[m+1][2];
-        for (int j =1; j < m ; j++){
-            B[j][0] = j * SequenceAlignment.GAP_PENALTY;
-        }
-
-        for (int j = 1; j < n; j++) {
-            B[0][1] = j * SequenceAlignment.GAP_PENALTY;
-            for (int i = 1; i < m; i++) {
-                B[i][1] = Math.min(Math.min(SequenceAlignment.MISMATCH_COST[ALPHABETS.indexOf(X.charAt(i - 1))][ALPHABETS.indexOf(Y.charAt(j -1))] + B[i + 1][j + 1],
-                        SequenceAlignment.GAP_PENALTY + B[i][j+1]), SequenceAlignment.GAP_PENALTY + B[i+1][j]);
-            }
-            for (int i = 1; i < m; i++) {
-                B[i][0] = B[i][1];
-            }
-        }
-
-        int[] R = new int[m];
-        for (int i = 0; i < m; i++) {
-            R[i] = B[i][1];
-        }
-        return R;
     }
 
 
-    public int[][] backwardSpaceEfficientAlignment_new(String X, String Y) {
+    public int[][] backwardSpaceEfficientAlignment(String X, String Y) {
         int m = X.length();
         int n = Y.length();
 
         int[][] B = new int[m][2];
-        for (int j = m-1 ; j >= 0 ; j--){
-            B[j][0] = (m-j) * SequenceAlignment.GAP_PENALTY;
+        for (int i = m - 1 ; i >= 0 ; i--){
+            B[i][0] = (m - i) * SequenceAlignment.GAP_PENALTY;
         }
 
-        for (int j = n-1; j >=0; j--) {
-            B[m-1][1] = (n-j) * SequenceAlignment.GAP_PENALTY;
-            System.out.println("X :: " + X);
-            System.out.println("Y :: " + Y);
-            for (int i = n-1; i >= 0; i--) {
-                B[i][1] = Math.min(Math.min(SequenceAlignment.MISMATCH_COST[ALPHABETS.indexOf(X.charAt(i))][ALPHABETS.indexOf(Y.charAt(j))] + B[i + 1][1],
-                        SequenceAlignment.GAP_PENALTY + B[i][1]), SequenceAlignment.GAP_PENALTY + B[i+1][0]);
+        for (int j = n - 1; j >= 0; j--) {
+            B[m - 1][0] = (n - j) * SequenceAlignment.GAP_PENALTY;
+
+            for (int i = m - 1; i >= 0; i--) {
+                B[i][0] = Math.min(
+                            Math.min(
+                                    SequenceAlignment.MISMATCH_COST[ALPHABETS.indexOf(X.charAt(i - 1))][ALPHABETS.indexOf(Y.charAt(j - 1))] + B[i + 1][1], SequenceAlignment.GAP_PENALTY + B[i + 1][0]
+                            ), SequenceAlignment.GAP_PENALTY + B[i][1]);
             }
-            for (int i = 1; i < m; i++) {
-                B[i][0] = B[i][1];
+
+            // Swap the columns to prepare for next iteration
+            for (int i = 0; i < m; i++) {
+                B[i][1] = B[i][0];
             }
         }
 
         return B;
-//        int[] R = new int[m];
-//        for (int i = 0; i < m; i++) {
-//            R[i] = B[i][1];
-//        }
-//        return R;
     }
+
+
+    /** Divide and Conquer - Efficient Implementation
+     * @param X
+     * @param Y
+     * @return
+     */
+    public void divideAndConquerAlignment(String X, String Y) {
+        int m = X.length();
+        int n = Y.length();
+
+        if(m <= 2 || n <= 2){
+            this.alignment(X,Y);
+        }
+
+        int[][] left = spaceEfficientAlignment(X, Y.substring(0, n / 2));
+        int[][] right = backwardSpaceEfficientAlignment(X, Y.substring(n / 2 + 1, n));
+
+        int min = left[1][1] + right[1][0];
+        int best_q = 1;
+        for (int q = 2; q < m ; q++) {
+            if (min > left[q][1] + right[q][0]){
+                min = left[q][1] + right[q][0];
+                best_q = q - 1;
+            }
+        }
+
+        System.out.println("Best q :: " + best_q);
+        P.add(new Pair(best_q, n/2));
+        divideAndConquerAlignment(X.substring(0,best_q), Y.substring(0, n/2));
+        divideAndConquerAlignment(X.substring(best_q+1,n), Y.substring(n/2 + 1, n));
+    }
+
 
     public String[] getAlignment(String x, String y) {
         int maxLength = x.length() + y.length();
@@ -331,7 +219,7 @@ public class SequenceAlignment {
         return new String[]{new String(xResult), new String(yResult)};
     }
 
-//
+
 //    public String[] getAlignmentDC(String x, String y) {
 //        int maxLength = x.length() + y.length();
 //
