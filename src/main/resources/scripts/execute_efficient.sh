e481b5b1d2dc2d352b85be57a0e67e55b59b887d
laxmigarde <laxmigarde95@gmail.com>
2021-12-06 17:58:18 -0800

    commit to add shell scripts

--- src/main/resources/scripts/execute_efficient.sh
+++ src/main/resources/scripts/execute_efficient.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-java -jar algo_project_efficient.jar input1.txt
+echo "Please enter the input filename:"
+read filename
+
+if [ -f "$filename" ]; then
+    java -jar algo_project_efficient.jar $filename
+else
+    echo "$filename does not exist."
+fi
